369a208ed31b279bac0cb917f62b7b1d3dde68ab
Deep Patel <dmpatel@andrew.cmu.edu>
2023-03-04 15:13:18 -0500

    code is running yay woohoo

--- data.py
+++ data.py
@@ -1,4 +1,6 @@
 import os
+from PIL import Image
+import numpy as np
 
 import torch
 import torch.nn as nn
@@ -11,57 +13,60 @@ from torchvision import transforms
 
 SPLITS = ["train", "val", "test"]
 DATA_DIR = "nerf_synthetic"
-CLASSES2IDX = {cls: i for i, cls in enumerate(
-    ["chair", "drums", "ficus", "hotdog", "lego", "materials", "mic", "ship"])}
+CLASSES = ["chair", "drums", "ficus", "hotdog", "lego", "materials", "mic", "ship"]
+CLASSES2IDX = {cls: i for i, cls in enumerate(CLASSES)}
 IDX2CLASSES = {i: cls for cls, i in CLASSES2IDX.items()}
 SIZE = 224
 
+
 class NERFDataset(Dataset):
-  def __init__(self, path, split):
-    self.path = path
-    self.split = split
-    self.size = SIZE
+    def __init__(self, path, split):
+        self.path = path
+        self.split = split
+        self.size = SIZE
 
-    self.imgPaths = []
-    self.labels = []
-    self.classPaths = {}
-    self.classCounts = {}
+        self.imgPaths = []
+        self.labels = []
+        self.classPaths = {}
+        self.classCounts = {}
 
-    for cls in os.listdir(self.path):
-      classPath = os.path.join(self.path, cls)
-      if os.path.isdir(classPath):
-        splitPath = os.path.join(classPath, self.split)
-        # print(os.listdir(splitPath))
-        imgPaths = [os.path.join(splitPath, filename) for filename in os.listdir(splitPath)]
-        [print(path) for path in imgPaths]
-        self.classPaths[cls] = imgPaths
-        self.labels += [cls for _ in imgPaths]
-        self.imgPaths += imgPaths
-        self.classCounts[cls] = len(imgPaths)
+        for cls in CLASSES:
+            classPath = os.path.join(self.path, cls)
+            if os.path.isdir(classPath):
+                splitPath = os.path.join(classPath, self.split)
+                imgPaths = [os.path.join(splitPath, filename) for filename in os.listdir(splitPath)]
 
+                self.classPaths[cls] = imgPaths
+                self.labels += [cls for _ in imgPaths]
+                self.imgPaths += imgPaths
+                self.classCounts[cls] = len(imgPaths)
 
-  def __len__(self):
-    return len(self.filePaths)
+    def __len__(self):
+        return len(self.imgPaths)
 
-  def __getitem__(self, idx):
-    imgPath = self.filePaths[idx]
+    def __getitem__(self, idx):
+        imgPath = self.imgPaths[idx]
+        img = Image.open(imgPath)
+        img = img.convert('RGB')
 
-    trans = transforms.Compose([
-        transforms.Resize((self.size, self.size)), # replace None with Resize Transformation to make shapes uniform,
-        transforms.ToTensor(), # convert the PIL Image object into a Tensor
-        transforms.ConvertImageDType(torch.float32),
-        transforms.Normalize(mean=[0.485, 0.457, 0.407], std=[0.5, 0.5, 0.5]), # replace None with a Normalize transform (mean=[0.485, 0.457, 0.407], std=[0.5, 0.5, 0.5])
-    ])
+        trans = transforms.Compose([
+            transforms.Resize((self.size, self.size)), # replace None with Resize Transformation to make shapes uniform,
+            transforms.ToTensor(), # convert the PIL Image object into a Tensor
+            transforms.ConvertImageDtype(torch.float32),
+            # replace None with a Normalize transform (mean=[0.485, 0.457, 0.407], std=[0.5, 0.5, 0.5])
+            transforms.Normalize(mean=[0.485, 0.457, 0.407], std=[0.5, 0.5, 0.5]),
+        ])
+        img = trans(img)
+        img = torch.FloatTensor(img)
 
-    img = trans(img)
-    img = torch.FloatTensor(img)
-    
-    numClasses = len(self.classCounts)
-    labelIdx = CLASSES2IDX[self.classPaths[idx]]
-    onehot = F.one_hot(torch.Tensor([labelIdx]).to(torch.int64), num_classes=numClasses)
+        numClasses = len(self.classCounts)
+        labelIdx = CLASSES2IDX[self.labels[idx]]
+        onehot = F.one_hot(torch.Tensor([labelIdx]).to(torch.int64),
+                           num_classes=numClasses).reshape((-1)).to(torch.float32)
 
-    return img, onehot
+        return img, onehot
 
 
 if __name__ == "__main__":
+    # print("collecting data")
     NERFDataset(DATA_DIR, SPLITS[0])
--- train.py
+++ train.py
@@ -20,71 +20,127 @@ IDX2CLASSES = {i: cls for cls, i in CLASSES2IDX.items()}
 SIZE = 224
 EPOCHS = 10
 
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        numChannels = 3
+        numClasses = len(CLASSES2IDX)
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=numChannels, out_channels=20, kernel_size=(5, 5)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=20, out_channels=30, kernel_size=(5, 5)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        )
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(in_channels=30, out_channels=50, kernel_size=(5, 5)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+            nn.Flatten(),
+        )
+
+        self.layer4 = nn.Sequential(
+            nn.Linear(in_features=28800, out_features=500),
+            nn.ReLU()
+        )
+
+        self.layer5 = nn.Sequential(
+            nn.Linear(in_features=500, out_features=numClasses),
+            nn.LogSoftmax(dim=1)
+        )
+
+    def forward(self, x):
+        yhat = self.layer1(x)
+        yhat = self.layer2(yhat)
+        yhat = self.layer3(yhat)
+        yhat = self.layer4(yhat)
+        yhat = self.layer5(yhat)
+        return yhat
+
+
 def createTrainingUtils(model):
-  trainDS = NERFDataset(SPLITS[0])
-  validDS = NERFDataset(SPLITS[1])
-  trainDL = DataLoader(trainDS, batch_size=32, shuffle=False)
-  validDL = DataLoader(validDS, batch_size=32, shuffle=False)
-  lossfn  = CrossEntropyLoss()
-  opt     = SGD(model.parameters(), lr=0.01, momentum=0.9) 
-  return model, trainDS, validDS, trainDL, validDL, lossfn, opt
+    trainDS = NERFDataset(DATA_DIR, SPLITS[0])
+    validDS = NERFDataset(DATA_DIR, SPLITS[1])
+    trainDL = DataLoader(trainDS, batch_size=32, shuffle=False)
+    validDL = DataLoader(validDS, batch_size=32, shuffle=False)
+    lossfn = CrossEntropyLoss()
+    opt = SGD(model.parameters(), lr=0.01, momentum=0.9)
+    return model, trainDS, validDS, trainDL, validDL, lossfn, opt
+
 
 def evaluateDL(model, dl, lossfn):
 
-  avgLoss = 0
-  acc = 0
-  for x, y in dl:
-    yhat = model(x)
-    
-    # compute the loss
-    loss += lossfn(yhat, y)
+    avgLoss = 0
+    acc = 0
+    for x, y in dl:
+        yhat = model(x)
 
-    # compute the number of correct samples
-    labelHat = torch.argmax(yhat, dim=1)
-    label = torch.argmax(y, dim=1)
-    acc += torch.sum(label == labelHat)
+        # compute the loss
+        loss += lossfn(yhat, y)
 
-  nsamples = len(dl.dataset)
-  avgLoss /= nsamples
-  acc /= nsamples
+        # compute the number of correct samples
+        labelHat = torch.argmax(yhat, dim=1)
+        label = torch.argmax(y, dim=1)
+        acc += torch.sum(label == labelHat)
 
-  return acc, avgLoss
+    nsamples = len(dl.dataset)
+    avgLoss /= nsamples
+    acc /= nsamples
+
+    return acc, avgLoss
+
 
 def evaluate(model, trainDL, validDL, lossfn):
 
-  with torch.no_grad():
-    trainLoss, trainAcc = evaluateDL(model, trainDL, lossfn)
-    validLoss, validAcc = evaluateDL(model, validDL, lossfn)
-    
-  return trainLoss, trainAcc, validLoss, validAcc
+    with torch.no_grad():
+        trainLoss, trainAcc = evaluateDL(model, trainDL, lossfn)
+        validLoss, validAcc = evaluateDL(model, validDL, lossfn)
+
+    return trainLoss, trainAcc, validLoss, validAcc
+
 
 def train(model, trainDL, validDL, lossfn, opt, epochs=EPOCHS):
 
-  trainLosses, trainAccs, validLosses, validAccs = [], [], [], []
-  
-  for e in range(EPOCHS):
-    for x, y in trainDL:
+    trainLosses, trainAccs, validLosses, validAccs = [], [], [], []
 
-      # forward pass
-      yhat = model(x)
+    for e in range(EPOCHS):
+        for x, y in trainDL:
 
-      # backward pass
-      opt.zero_grad()
-      loss = lossfn(y, yhat)
-      loss.backward()
-      opt.step()
+            # forward pass
+            yhat = model(x)
 
-    # evaluate
-    trainLoss, trainAcc, validLoss, validAcc = evaluate(model, trainDL, validDL)
-    trainLosses.append(trainLosses)
-    trainAccs.append(trainAcc)
-    validLosses.append(validLoss)
-    validAccs.append(validAccs)
+            # backward pass
+            opt.zero_grad()
+            loss = lossfn(yhat, y)
+            loss.backward()
+            opt.step()
 
-  return model, trainLoss, trainAcc, validLoss, validAcc
+        # evaluate
+        trainLoss, trainAcc, validLoss, validAcc = evaluate(model, trainDL, validDL)
+        trainLosses.append(trainLosses)
+        trainAccs.append(trainAcc)
+        validLosses.append(validLoss)
+        validAccs.append(validAcc)
+
+        print(trainLoss, trainAcc, validLoss, validAcc)
+
+    return model, trainLoss, trainAcc, validLoss, validAcc
+
 
 def main():
-    train()
+    model = CNN()
+    model, trainDS, validDS, trainDL, validDL, lossfn, opt = createTrainingUtils(model)
+
+    train(model, trainDL, validDL, lossfn, opt)
+
 
 if __name__ == "__main__":
     main()
